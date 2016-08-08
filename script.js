7b7abdde86317f8fcaa09adafe10cedd9097424a
Saumya Aggarwal <aggarwal.saumya27@gmail.com>
2016-08-08 23:27:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,33 +4,33 @@ var context = $cnvs.getContext('2d');
 var centerX = $cnvs.width / 2;
 var centerY = $cnvs.height / 2;
 
-var radius = 30;
-
-context.beginPath();
-context.arc(centerX,centerY,radius,0,2*Math.PI);
-context.fillStyle = '#DEC0DE';
-context.fill();
-context.lineWidth = 2;
-context.strokeStyle = '#003300';
-context.stroke();
-context.beginPath();
+var radius = 30, count = 1 ;
 
 $('.btn-sbmt').on('click', function(){
 
-    var $name = $('.name').val();
-    var $string = $('.string').val();
+    context.clearRect(0, 0, $cnvs.width, $cnvs.height);
 
-    context.beginPath();
-    context.font = '8pt Calibri';
-    context.fillStyle = "red";
-    context.textAlign = 'center';
-    context.fillText($name, centerX, centerY);
-    context.fill();
+    var $name = $('.name').val(),
+    $string = $('.string').val(),
+    degree_increment = 360/$string.length, degree = 0;
 
-    
     for(var i = $string.length-1; i>=0; i--){
+        var rndm = Math.floor(Math.random()*($cnvs.height/2-150))+100
+
+        var circle_x = centerX + rndm * Math.cos(Math.PI * degree / 180.0),
+        circle_y = centerY + rndm * Math.sin(Math.PI * degree / 180.0),
+        radius += 5*count;
+
+
+        if($name.indexOf($string[i]) > -1){
+            context.beginPath();
+            context.moveTo(circle_x,circle_y);
+            context.lineTo(centerX,centerY);
+            context.stroke();    
+        }
+
         context.beginPath();
-        context.arc(i*200,i*250,radius,0,2*Math.PI);
+        context.arc(circle_x,circle_y,radius,0,2*Math.PI);
         context.fillStyle = getRandomNewColor();
         context.fill();
         context.lineWidth = 2;
@@ -41,14 +41,27 @@ $('.btn-sbmt').on('click', function(){
         context.font = '10pt Calibri';
         context.fillStyle = "BLACK";
         context.textAlign = 'center';
-        context.fillText($string[i], i*200, i*250);
+        context.fillText($string[i], circle_x, circle_y);
         context.fill();
-
-        context.beginPath();
-        context.moveTo(i*200+radius,i*250);
-        context.lineTo(centerX-radius,centerY);
-        context.stroke();
+        degree += degree_increment;
     }
+
+    context.beginPath();
+    context.arc(centerX,centerY,radius,0,2*Math.PI);
+    context.fillStyle = '#DEC0DE';
+    context.fill();
+    context.lineWidth = 2;
+    context.strokeStyle = '#003300';
+    context.stroke();
+    context.beginPath();
+
+    context.beginPath();
+    context.font = '8pt Calibri';
+    context.fillStyle = "red";
+    context.textAlign = 'center';
+    context.fillText($name, centerX, centerY);
+    context.fill();
+
 
 });
 
