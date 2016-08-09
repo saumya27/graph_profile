81981dd84e93f2044325ad1f098b15aaaeb7a566
Saumya Aggarwal <aggarwal.saumya27@gmail.com>
2016-08-09 17:28:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-// Canvas v=baic properties
+// Canvas baic properties
 var $cnvs = document.getElementById("cnvs"),
 context = $cnvs.getContext('2d'),
 centerX = $cnvs.width / 2,
 centerY = $cnvs.height / 2;
 
 // click handler
-$('.btn-sbmt').on('click', function(){
-    var radius = 30;
+$('.btn-sbmt').on('click', function(){    
     context.clearRect(0, 0, $cnvs.width, $cnvs.height);
-
-    var $name = $('.name').val(),
-    $string = $('.string').val(),
-    degree_increment = 360/$string.length, degree = 0,
+    var $name = $('.name').val().toUpperCase(),
+    $string = $('.string').val().toUpperCase(),
+    degree = 0,
     letters =  new Object;
 
     // make the letters and count map
@@ -19,11 +17,12 @@ $('.btn-sbmt').on('click', function(){
         var char = $string[x];
         letters[char] = (isNaN(letters[char]) ? 1 : letters[char] + 1);
     }
-
+    var degree_increment = 360/(Object.keys(letters).length);
     // loop in the characters of a string
     for(key in letters) {
+        var radius = 10;
         console.log(key + ' :: ' + letters[key]);
-        var rndm = Math.floor(Math.random()*($cnvs.height/2-150))+100;
+        var rndm = Math.floor(Math.random()*($cnvs.height/2-200))+150;
 
         var circle_x = centerX + rndm * Math.cos(Math.PI * degree / 180.0),
         circle_y = centerY + rndm * Math.sin(Math.PI * degree / 180.0);
@@ -35,6 +34,7 @@ $('.btn-sbmt').on('click', function(){
             context.beginPath();
             context.moveTo(circle_x,circle_y);
             context.lineTo(centerX,centerY);
+            context.strokeStyle = '#000';
             context.stroke();    
         }
 
@@ -49,7 +49,7 @@ $('.btn-sbmt').on('click', function(){
 
         //write the character value in the circle
         context.beginPath();
-        context.font = '10pt Calibri';
+        context.font = '10pt Helvetica';
         context.fillStyle = "BLACK";
         context.textAlign = 'center';
         context.fillText(key, circle_x, circle_y);
@@ -58,19 +58,12 @@ $('.btn-sbmt').on('click', function(){
         degree += degree_increment;
     }
 
-    // make the center circle
-    context.beginPath();
-    context.arc(centerX,centerY,50,0,2*Math.PI);
-    context.fillStyle = '#DEC0DE';
-    context.fill();
-    context.lineWidth = 2;
-    context.strokeStyle = '#003300';
-    context.stroke();
-    context.beginPath();
+    // make the center star
+    drawNameStar(centerX,centerY,7,80,45);
 
     //write the name in center circle
     context.beginPath();
-    context.font = '8pt Calibri';
+    context.font = '10pt Helvetica';
     context.fillStyle = "red";
     context.textAlign = 'center';
     context.fillText($name, centerX, centerY);
@@ -92,3 +85,31 @@ function getRandomNewColor() {
         getRandomNewColor();
     }
 } 
+
+function drawNameStar(cx,cy,spikes,outerRadius,innerRadius){
+  var rot=Math.PI/2*3;
+  var x=cx;
+  var y=cy;
+  var step=Math.PI/spikes;
+
+  context.beginPath();
+  context.moveTo(cx,cy-outerRadius)
+  for(i=0;i<spikes;i++){
+    x=cx+Math.cos(rot)*outerRadius;
+    y=cy+Math.sin(rot)*outerRadius;
+    context.lineTo(x,y)
+    rot+=step
+
+    x=cx+Math.cos(rot)*innerRadius;
+    y=cy+Math.sin(rot)*innerRadius;
+    context.lineTo(x,y)
+    rot+=step
+  }
+  context.lineTo(cx,cy-outerRadius);
+  context.closePath();
+  context.lineWidth=5;
+  context.strokeStyle='blue';
+  context.stroke();
+  context.fillStyle='skyblue';
+  context.fill();
+}
